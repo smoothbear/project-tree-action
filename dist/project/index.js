da40d2298451b4dccd0c443b648c162da9c4eb48
KIM JUNG BIN <kjbin0420@naver.com>
2022-07-04 21:55:12 +0900

    feat: dist

--- dist/project/index.js
+++ dist/project/index.js
@@ -7368,26 +7368,26 @@ const git = (0, simple_git_1.default)(baseDir);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         const regex = core.getInput("regex", { required: true });
-        const path = core.getInput("path", { required: true });
+        const path = `${process.env.GITHUB_WORKSPACE}/${core.getInput("path", { required: true })}`;
         const message = core.getInput("message", { required: true });
         const email = core.getInput("email", { required: true });
         const username = core.getInput("username", { required: true });
         (0, child_process_1.exec)("tree -a", (error, stdout, stderr) => {
             if (error) {
-                core.error(`error: ${error.message}`);
+                core.error(`error: exec ${error.message}`);
                 return;
             }
             if (stderr) {
-                core.error(`stderr: ${stderr}`);
+                core.error(`stderr: stderr ${stderr}`);
                 return;
             }
             fs.readFile(path, "utf-8", (err, contents) => {
                 if (err) {
-                    core.error(`error: ${err}`);
+                    core.error(`error: readfile ${err}`);
                     return;
                 }
                 const replaced = contents.replace(RegExp(regex), stdout);
-                fs.writeFile(path, replaced, "utf-8", (err) => core.error(`error: ${err}`));
+                fs.writeFile(path, replaced, "utf-8", (err) => core.error(`error: writefile ${err}`));
             });
             git.addConfig("user.email", email).addConfig("user.name", username);
             git.add(path);
